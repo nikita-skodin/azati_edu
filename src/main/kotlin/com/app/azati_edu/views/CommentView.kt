0e94a47cf121d4f72e2fc72f05fcedf19ab2eb8b
skodin <skodin36@gmail.com>
2024-12-17 17:31:27 +0300

    add Views

---
+++ src/main/kotlin/com/app/azati_edu/views/CommentView.kt
@@ -0,0 +1,19 @@
+package com.app.azati_edu.views
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class CommentView(
+    var content: String,
+    var userId: Long,
+    var postId: Long,
+) : Serializable {
+    var id: Long? = null
+    var createdAt: LocalDateTime? = null
+    var updatedAt: LocalDateTime? = null
+}
---
+++ src/main/kotlin/com/app/azati_edu/views/UserView.kt
@@ -0,0 +1,18 @@
+package com.app.azati_edu.views
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class UserView(
+    var username: String,
+    var email: String,
+) : Serializable {
+    var id: Long? = null
+    var createdAt: LocalDateTime? = null
+    var updatedAt: LocalDateTime? = null
+}
