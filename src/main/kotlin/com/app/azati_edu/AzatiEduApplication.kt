a81bbded7bb64736c2cc57bd26a6cbf12be40c5a
skodin <skodin36@gmail.com>
2024-12-18 11:40:41 +0300

    add loggerFactory method

--- src/main/kotlin/com/app/azati_edu/AzatiEduApplication.kt
+++ src/main/kotlin/com/app/azati_edu/AzatiEduApplication.kt
@@ -1,5 +1,7 @@
 package com.app.azati_edu
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
@@ -10,4 +12,8 @@ class AzatiEduApplication
 
 fun main(args: Array<String>) {
     runApplication<AzatiEduApplication>(*args)
+}
+
+inline fun <reified T> getClassLogger(): Logger {
+    return LoggerFactory.getLogger(T::class.java)
 }
